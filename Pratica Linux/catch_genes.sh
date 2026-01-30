78e0a3d69e2e799c80f4cf297c4faa5576071ad8
Felipe Simionato Salles <123026352+211295@users.noreply.github.com>
2026-01-29 22:18:46 -0300

    Add catch_genes.sh for FASTA sequence extraction
    This script processes FASTA files to extract sequences based on a list of IDs.

---
+++ Pratica Linux/catch_genes.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+for file in ;
+do
+perl -e '
+($id,$fasta)=@ARGV;
+open(ID,$id);
+while (<ID>) {
+    s/\r?\n//;
+    /^>?(\S+)/;
+    $ids{$1}++;
+}
+$num_ids = keys %ids;
+open(F, $fasta);
+$s_read = $s_wrote = $print_it = 0;
+while (<F>) {
+    if (/^>(\S+)/) {
+	$s_read++;
+	if ($ids{$1}) {
+	    $s_wrote++;
+	    $print_it = 1;
+	    delete $ids{$1}
+	}
+	else {
+	    $print_it = 0
+	}
+
+    };
+    if ($print_it) {
+	print $_
+    }
+};
+END {
+    warn "Searched $s_read FASTA records.\nFound $s_wrote IDs out of $num_ids in the ID list.\n"
+}
+' Genes_total $file > Gene_of_specie_$file;
+done
