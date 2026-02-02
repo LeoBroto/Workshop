d79944f764b5774254ff959115a5c4ff450862c0
Felipe Simionato Salles <123026352+211295@users.noreply.github.com>
2026-02-02 11:12:49 -0300

    Fix comment spelling in fasta_length.sh
    Updated comment to correct spelling and improve clarity.

--- Pratica Linux/fasta_length.sh
+++ Pratica Linux/fasta_length.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-### Count othe length of sequencies in a fasta file
+###length of sequencies in a fasta file
 
 for file in *.fasta;
 do awk '/^>/ {if (seqlen){print seqlen}; print ;seqlen=0;next; } { seqlen += length($0)}END{print seqlen}' $file > length_of_$file;
